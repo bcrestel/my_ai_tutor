848a96fe172144e469a7559a8aa0f2aae2fd9c18
Ben Crestel <ben.crestel@zoho.com>
2023-09-26 23:00:35 -0400

    add cropping capability

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -2,7 +2,10 @@ from enum import Enum
 import logging
 
 import streamlit as st
+from streamlit_cropper import st_cropper
 from PIL import Image
+
+from src.frontend.utils import InputMode
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -10,14 +13,15 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-class InputMode(Enum):
-    take = "Prendre une photo"
-    load = "Verser une photo"
+if "crop" not in st.session_state:
+    st.session_state.crop = False
+    st.session_state.cropped_image = None
 
 st.sidebar.header("Méthode d'ajout de la photo")
 input_mode = st.sidebar.selectbox(
     "Choisis comment tu veux ajouter une photo de ton texte:",
-    [InputMode.take.value, InputMode.load.value]
+    [InputMode.load.value]
+    #[InputMode.take.value, InputMode.load.value]
 )
 
 if input_mode == InputMode.take.value:
@@ -29,25 +33,35 @@ original_picture = st.empty()
 cropped_picture = st.empty()
 
 if picture is not None:
-    original_picture.image(picture, caption="Photo de ton texte originale", use_column_width=True)
-
-    # Define cropping parameters
-    st.sidebar.header("Crop Parameters")
-    left = st.sidebar.number_input("Left", value=0, min_value=0)
-    top = st.sidebar.number_input("Top", value=0, min_value=0)
-    right = st.sidebar.number_input("Right", value=0, min_value=0)
-    bottom = st.sidebar.number_input("Bottom", value=0, min_value=0)
-
     if st.sidebar.button("Crop"):
-        # Open and crop the image using PIL
+        st.session_state.crop = True
+
+    if st.session_state.crop:
+        cropped_picture.image(st.session_state.cropped_image, caption="Photo de ton texte croquée", use_column_width=True)
+    else:
+        #original_picture.image(picture, caption="Photo de ton texte originale", use_column_width=True)
         image = Image.open(picture)
-        width, height = image.size
-        logger.debug(f"left={left}, top={top}, right={right}, bottom={bottom}")
-        cropped_image = image.crop((left, top, right, bottom))
-        # Display the cropped image
-        cropped_picture.image(cropped_image, caption="Photo de ton texte croquée", use_column_width=True)
+        st.session_state.cropped_image = st_cropper(image)
+
+#    # Define cropping parameters
+#    st.sidebar.header("Crop Parameters")
+#    left = st.sidebar.number_input("Left", value=0, min_value=0)
+#    top = st.sidebar.number_input("Top", value=0, min_value=0)
+#    right = st.sidebar.number_input("Right", value=0, min_value=0)
+#    bottom = st.sidebar.number_input("Bottom", value=0, min_value=0)
+#
+#    if st.sidebar.button("Crop"):
+#        # Open and crop the image using PIL
+#        image = Image.open(picture)
+#        width, height = image.size
+#        logger.debug(f"left={left}, top={top}, right={right}, bottom={bottom}")
+#        cropped_image = image.crop((left, top, right, bottom))
+#        # Display the cropped image
+#        cropped_picture.image(cropped_image, caption="Photo de ton texte croquée", use_column_width=True)
     
 if st.sidebar.button("Recommencer"):
     original_picture.empty()
     cropped_picture.empty()
     picture = None
+    st.session_state.crop = False
+    st.session_state.cropped_image = None
---
+++ src/frontend/utils.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class InputMode(Enum):
+    take = "Prendre une photo"
+    load = "Verser une photo"
