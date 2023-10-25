a90d5b670073c93b8281e427ed47153e2fc36fb5
Ben Crestel <ben.crestel@zoho.com>
2023-10-24 22:58:44 -0400

    add doc

--- src/backend/plot_box.py
+++ src/backend/plot_box.py
@@ -2,11 +2,19 @@ from PIL import ImageFile, ImageDraw
 import pandas as pd
 
 
-def plot_box(img: ImageFile.ImageFile, df_pytesseract: pd.DataFrame):
+def plot_box(img: ImageFile.ImageFile, df_pytesseract: pd.DataFrame, fill=None, outline="red"):
+    """Plot boxes as defined by pytesseract on img
+
+    Args:
+        img (ImageFile.ImageFile): input image
+        df_pytesseract (pd.DataFrame): pandas.DataFrame produced by pytesseract.image_to_data
+        fill (_type_, optional): _description_. Defaults to None.
+        outline (str, optional): _description_. Defaults to "red".
+    """
     img_box = ImageDraw.Draw(img)
     all_dim = df_pytesseract[["left", "top", "width", "height"]].values
     for dim in all_dim:
-        add_rectangle(img_box, *dim)
+        add_rectangle(img_box, *dim, fill=fill, outline=outline)
     img.show()
 
 
